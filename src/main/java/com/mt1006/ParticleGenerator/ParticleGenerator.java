392e1baf6d161bce75ce563b04f7f2f95f5c8519
mt1006 <damianm916@gmail.com>
2023-02-19 21:12:57 +0100

    Fixed version number

--- src/main/java/com/mt1006/ParticleGenerator/ParticleGenerator.java
+++ src/main/java/com/mt1006/ParticleGenerator/ParticleGenerator.java
@@ -9,7 +9,7 @@ public class ParticleGenerator implements ModInitializer
 {
 	public static final String MOD_ID = "pgen";
 	public static final String VERSION = "1.0.1";
-	public static final String MOD_FOR_VERSION = "1.18.2";
+	public static final String MOD_FOR_VERSION = "1.19.2";
 	public static final String MOD_FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
 
