9f8556c5ddbd27a5f01c6e49a8aaa1f51efb6f05
mt1006 <damianm916@gmail.com>
2023-03-15 18:46:57 +0100

    Version for 1.19.4

--- src/main/java/com/mt1006/ParticleGenerator/ParticleGenerator.java
+++ src/main/java/com/mt1006/ParticleGenerator/ParticleGenerator.java
@@ -9,7 +9,7 @@ public class ParticleGenerator implements ModInitializer
 {
 	public static final String MOD_ID = "pgen";
 	public static final String VERSION = "1.0.1";
-	public static final String MOD_FOR_VERSION = "1.19.3";
+	public static final String MOD_FOR_VERSION = "1.19.4";
 	public static final String MOD_FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
 
